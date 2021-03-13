7b42f8a63d26721b450487ca09ec6570a965426d
Mohnish Thandassery <68819343+mohnishthandassery@users.noreply.github.com>
2021-03-13 21:01:54 +0530

    Format index.js

--- pages/index.js
+++ pages/index.js
@@ -24,14 +24,14 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-  <Link href={`/posts/${id}`}>
-    <a>{title}</a>
-  </Link>
-  <br />
-  <small className={utilStyles.lightText}>
-    <Date dateString={date} />
-  </small>
-</li>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
           ))}
         </ul>
       </section>
